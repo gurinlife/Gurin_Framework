bd68c402f93d42ab1b64d8d24e9b552b7d19f1da
unknown <gurinlife@gmail.com>
2017-08-01 22:12:40 +0700

    fixing bug in index

--- index.php
+++ index.php
@@ -3,6 +3,4 @@
 include('init.php');
 
 $current_url = (isset($_SERVER['HTTPS'])) ? 'https' : 'http'.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$path        = parse_url($current_url, PHP_URL_PATH);
-
-dump($path);
+$path        = explode('/', trim(parse_url($current_url, PHP_URL_PATH), '/'));
