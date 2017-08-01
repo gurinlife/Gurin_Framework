cedf1a496eab2859169ab56af628e075a9f4c761
unknown <gurinlife@gmail.com>
2017-08-01 22:07:36 +0700

    add current url

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 include('init.php');
 
-$path = parse_url($url, PHP_URL_PATH);
+$current_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']";
+$path = parse_url($current_url, PHP_URL_PATH);
 
 dump($path);
